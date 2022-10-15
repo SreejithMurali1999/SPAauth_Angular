035fb61fdcd38f869f71d3dd9fcf2cd2170aa29c
SreejithMurali1999 <sreejithnoble1999@gmail.com>
2022-10-15 13:15:36 +0530

    My_SPA_Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+
+const appRoute:Routes = [
+  {path:'',component:SignInComponent},
+  {path:'Create',component:RegisterComponent},
+  {path:'sign-in',component:SignInComponent}
+  
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    RegisterComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,RouterModule.forRoot(appRoute),ReactiveFormsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
